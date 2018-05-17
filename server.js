a2b88db1c8a9f256f46c4f6b295d2849c6e481c8
CTortosaM <CTortosaM@users.noreply.github.com>
2018-05-17 16:11:57 +0200

    Implementación gráfica

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ servidor.get("/", function(peticion, respuesta) {
 
 servidor.post('/cambioContrasenya', cambiarContrasenya);
 
-servidor.get('/zona', getZona)
+servidor.get('/zona', getZonas)
 
 servidor.get('/sensores', getSensores)
 
@@ -37,12 +37,14 @@ servidor.post('/volverUsuarioActivo', turnUsuarioActivo);
 
 servidor.get('/recuperarContrasenya', enviarContrasenyaDeRecuperacion);
 
+servidor.get('/getMedidas', getMedidas);
+
 //servidor.get('/lobby', procesarUsuario);
 //BASE DATOS
 var sqlite3 = require('sqlite3');
 base_datos = new sqlite3.Database('base_datos.db', function(err) {
   if (err != null) {
-    respuesta.sendStatus(503);
+    console.log(err);
   }
 });
 //-----------------------------------------------------------------------------
@@ -79,7 +81,7 @@ function procesar_login(peticion, respuesta) {
       } //else
     } //else
   } //procesar_login2
-  base_datos.get('SELECT * FROM usuarios WHERE email=?', [peticion.query.email], procesar_login2);
+  base_datos.get('SELECT nombre, apellidos, email, sexo, empresa, permisos, activo FROM usuarios WHERE email=?', [peticion.query.email], procesar_login2);
 
 } //procesarLogin
 
@@ -99,27 +101,31 @@ function cambiarContrasenya(peticion, respuesta) {
 
 //--------------------------------------------------------------------------------
 //FUNCIÓN QUE DEVUELVE LAS ZONAS SEGÚN LA ID DE ZONA
-function getZona(peticion, respuesta) {
-  var objZona = {};
-  var zona;
-  var vertices;
-  base_datos.get('SELECT * from Zona WHERE id=' + peticion.query.id, function(err, res) {
-    if (err != null) {
-      respuesta.sendStatus(500);
-    } else {
-      base_datos.all('SELECT * from Vertice WHERE zonaId=' + peticion.query.id, function(error, array) {
-        if (error != null) {
-          respuesta.sendStatus(500);
-        } else {
-          respuesta.send({
-            zona: res,
-            vertices: array
-          }) //send
-        } //else
-      }) //base_datos.all
-    } //else
-  }) //base_datos.get
+function getZonas(peticion, respuesta) {
+  var errores = 0;
+  var objZonas = {};
+  var zonas = [];
+  base_datos.all('SELECT nombre, color, id from Zona WHERE userID=' + peticion.query.id,
+    function(err, zones) {
+      if (err) {
+        respuesta.sendStatus(500);
+      } else {
+        zonas = zones;
+        for (let zone of zonas) {
+          base_datos.all('SELECT lat, lng from Vertice WHERE zonaId=' + zone.id,
+            function(err2, vertex) {
+              if (err2) {
+                return;
+              } else {
+                zone.vertices = vertex;
+              } //else
+            }) //base_datos.all
+        } //for
+      } //else
+    }) //base_datos.all
 } //getZona
+
+/* PENDIENTE DE REVISIÓN*/
 
 //----------------------------------------------------------------------------------
 //FUNCIÓN QUE DEVUELVE LOS SENSORES DE LA BASE DE datos
@@ -142,6 +148,27 @@ function turnUsuarioActivo(peticion, respuesta) {
     }
   });
 }
+//-----------------------------------------------------------------------------
+//FUNCIÓN PARA RECIBIR UN ARRAY CON LAS MEDIDAS DE UNA MAGNITUD
+function getMedidas(peticion, respuesta) {
+  base_datos.all('SELECT tiempo FROM medidas', function(err1, tiempos) {
+    if (err1) {
+      respuesta.sendStatus(404);
+    } else {
+      base_datos.all('SELECT ' + peticion.query.magnitud + ' FROM medidas', function(err2, datos) {
+        if (err2) {
+          respuesta.sendStatus(404);
+        } else {
+          let response = {
+            abcisa: tiempos,
+            ordenada: datos
+          } //response
+          respuesta.send(response);
+        } //else
+      }) //base_datos.all
+    } //else
+  }) //base_datos.all
+} //getMedidas
 //------------------------------------------------------------------------------------
 // FUNCIÓN PARA ENVIAR UNA CONTRASENYA DE RECUPERACION SI EL USUARIO LA HA PERDIDO
 function enviarContrasenyaDeRecuperacion(peticion, respuesta) {
@@ -163,7 +190,7 @@ function enviarContrasenyaDeRecuperacion(peticion, respuesta) {
   var mailOptions = {
     from: 'softfields@gmail.com', // sender address
     to: emailDestino, // list of receivers
-    subject: 'Solicitud de nueva contraseña', // Subject line
+    subject: 'Solicitud de nueva contrasenya', // Subject line
     text: texto //, // plaintext body
     // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
   }
@@ -186,13 +213,12 @@ function enviarContrasenyaDeRecuperacion(peticion, respuesta) {
                   yo: 'error'
                 });
               } else {
-                console.log('Message sent: ' + info.response);
                 respuesta.json({
                   yo: info.response
-                });
-              };
-            });
-          }
+                }); //json
+              }; //else
+            }); //sendMail
+          } //else
         }); //transportista
       } //if
     } //if
